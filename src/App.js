35f461a4bf2a4478fa722c34c75be7210b862589
Manel-Hik <manelaloui2@gmail.com>
2021-03-31 10:08:00 +0100

    commiting state_checkpoint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'; 
+import  { Component } from 'react'
+import profile_picture from './manel.jpeg'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      person:{
+      fullName:"Manel ALOUI",
+      bio:"Hi guys, I'm Manel ALOUI, an engineer who loves writing novels, You are welcome to visit my profile!!",
+      profession:"Junior data scientist and a full stack web developer",
+      imgSrc:profile_picture,
+
+      },
+      show: false,
+      counte:0
+    }
+  }
+   show_profile = ()=> {
+    this.setState({show:!this.state.show})
+  }
+  componentDidMount(){
+    let startCount = setInterval(()=>{ this.setState({counte:this.state.counte+1})
+  });
+  }
+  render() {
+    return (
+      <div>
+        { this.state.show ? <div className="profile-card"><h1 className="esmi"> My name is {this.state.person.fullName}</h1>
+        <h2>{this.state.person.bio}</h2>
+        <h3>I'm {this.state.person.profession}</h3>
+        <img  style={{borderRadius: "100%", width: "200px", height: "200px"}}  src={this.state.person.imgSrc}/>    
+             </div>
+      : null}
+        <button className="btn"
+         onClick={this.show_profile}> 
+         You are welcome!
+         </button>
+         <p>{this.state.counte}</p>
+      </div>
+    )
+  }
 }
+
+
+
 
 export default App;
